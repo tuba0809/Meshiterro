1b48903c3c9c0150c55a393b539523aa5fa5ea73
tuba0809 <tubasa8342849@gmail.com>
2021-08-17 13:31:36 +0000

    day2

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,16 +1,33 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
   end
 
   def create
+    @post_images = PostImage.new(post_image_params)
+    @post_images.user_id = current_user.id
+    @post_images.save
+    redirect_to post_images_path
   end
 
   def index
+     @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
+     @post_image = PostImage.find(params[:id])
+     @post_comment = PostComment.new
   end
 
   def destroy
-  endcd 
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+   private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2dc9dd6a46291c52eeb6f36117912b7a252ff2e27198d8ca5b78615f660da2362b6c52300b5d634020dcc185f03d8f700ade29cfc11b6875ea0b5525e71a2b04'
